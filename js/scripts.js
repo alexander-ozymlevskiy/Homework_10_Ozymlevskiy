c26a9d9fe1f055faebcba4f53e9201596784bfe9
Александр Озимлевский <alex@MacBook-Air-Aleksandr.local>
2023-04-21 17:20:39 +0300

    fixed the button, changed the right arrow icon, corrected the icon font code

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,27 @@
-
 $(document).ready(function () {
-    $(".banner-slick").slick({
-      dots: true,
-      arrows: false
-    });
+  $(".banner-slick").slick({
+    dots: false,
+    arrows: false,
+  });
+});
+
+document
+  .querySelector(".button-download-psd")
+  .addEventListener("click", function () {
+    var url = "../img/file.png";
+    var filename = "file.png";
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", url, true);
+    xhr.responseType = "blob";
+    xhr.onload = function () {
+      var blob = xhr.response;
+      var a = document.createElement("a");
+      var url = window.URL.createObjectURL(blob);
+      a.href = url;
+      a.download = filename;
+      document.body.appendChild(a);
+      a.click();
+      window.URL.revokeObjectURL(url);
+    };
+    xhr.send();
   });
